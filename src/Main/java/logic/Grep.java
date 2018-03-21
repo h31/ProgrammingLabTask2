352a7da57b260add078f0a57d617d7d37b73a69c
Nikita.Bochkarev <nikita280799@mail.ru>
2018-03-21 21:05:24 +0300

    Some methods

--- src/Main/java/logic/Grep.java
+++ src/Main/java/logic/Grep.java
@@ -3,11 +3,12 @@ package logic;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Grep {
 
-    private  List<String> text;
+    private List<String> text = new ArrayList<String>();
 
     public Grep(String inputDirectory) {
         try {
@@ -17,4 +18,43 @@ public class Grep {
         }
     }
 
+    public List<String> grepWithWord(String word) {
+        List<String> output = new ArrayList<String>();
+        for (String s:text) {
+            if (s.contains(word)) {
+                output.add(s);
+            }
+        }
+        return output;
+    }
+
+    public List<String> grepWithRegex(String pattern) {
+        List<String> output = new ArrayList<String>();
+        for (String s:text) {
+            if (s.matches(pattern)) {
+                output.add(s);
+            }
+        }
+        return output;
+    }
+
+    public List<String> grepWithoutRegex(String pattern) {
+        List<String> output = new ArrayList<String>();
+        for (String s:text) {
+            if (!s.matches(pattern)) {
+                output.add(s);
+            }
+        }
+        return output;
+    }
+
+    public List<String> grepWithIgnoreRegister(String pattern) {
+        List<String> output = new ArrayList<String>();
+        for (String s:text) {
+            if (s.matches("?" + pattern)) {
+                output.add(s);
+            }
+        }
+        return output;
+    }
 }
