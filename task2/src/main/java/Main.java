9d42c42040d8e5bf977b3e83aac30a1c67c0e922
Ferrero <myfn@yandex.ru>
2018-03-24 21:28:28 +0300

    Handmade parser, tests for parser

--- task2/src/main/java/Main.java
+++ task2/src/main/java/Main.java
@@ -1,25 +1,17 @@
+import UI.Parsers.Args;
 import logic.Operations.Collector;
 import logic.Operations.Separator;
 
-import java.io.File;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args){
-        File file = new File("/Users/Ferrero/IdeaProjects/ProgrammingLabTask2/task2/src/main/resources/test.txt");
-        /*
-        Collector collector = new Collector();
-        File testFile1 = new File("/Users/Ferrero/IdeaProjects/ProgrammingLabTask2/task2/src/main/resources/test1.txt");
-        File testFile2 = new File("/Users/Ferrero/IdeaProjects/ProgrammingLabTask2/task2/src/main/resources/test2.txt");
-        File testFile3 = new File("/Users/Ferrero/IdeaProjects/ProgrammingLabTask2/task2/src/main/resources/test3.txt");
-        List testFiles = Arrays.asList(testFile1, testFile2, testFile3);
-
-        collector.collectFile(testFiles, "testFile.txt");
-*/
-        Separator separator = new Separator();
-        separator.separateFile(file);
-
-
+    public static void main(String[] args) throws Exception {
+        Args arvs = new Args(args);
+        arvs.getTask();
+        if (arvs.taskFlag()) {
+            Collector collector = new Collector();
+            collector.collectFile(arvs.getFilesToCollect(), arvs.getCollectionFile().getName());
+        } else {
+            Separator separator = new Separator();
+            separator.separateFile(arvs.getFileToSeparate());
+        }
     }
 }
