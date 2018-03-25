fd6c7a673c26a68812a5aef467c9a179dfc12c9e
Yulia <myk.99@yandex.ru>
2018-03-25 16:44:30 +0300

    Дописала cli

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
 import org.apache.commons.cli.*;
 
+import java.io.File;
+
 public class Main {
-    class Cli{
+    private static Cli c = new Cli();
+    static class Cli{
         private Options options = new Options();
         public Cli(){
             options.addOption(new Option("c", "crypt", true, "key for encription"));
@@ -10,17 +13,47 @@ public class Main {
         }
 
         public void parse(String args[]){
-            CommandLineParser prs = new PosixParser();
-
+            int type = 0;
+            String key = "";
+            String out = null;
+            String in = "";
+            CommandLineParser prs = new DefaultParser();
             try {
                 CommandLine cmd = prs.parse(options,args);
-
+                if(cmd.hasOption('c')) {
+                    type = 1;
+                    key = cmd.getOptionValue('c');
+                }
+                if(cmd.hasOption('d')){
+                    if (type == 0)
+                        type = 2;
+                    key = cmd.getOptionValue('d');
+                }
+                if(cmd.hasOption('o'))
+                    out = cmd.getOptionValue('o');
+                int i = 0;
+                while (i < cmd.getArgList().size()){
+                    if(cmd.getArgList().get(i).equals("-d")
+                            || cmd.getArgList().get(i).equals("-c")
+                            || cmd.getArgList().get(i).equals("-o"))
+                        i += 2;
+                    else
+                        in = cmd.getArgList().get(i);
+                }
+                File fl = new File(in);
+                if(!fl.isFile()) throw new IllegalArgumentException();
+                if(type == 0) throw new IllegalArgumentException();
             } catch (ParseException e) {
-                e.printStackTrace();
+                HelpFormatter hp = new HelpFormatter();
+                hp.printHelp("ciphxor",options);
+            } catch (IllegalArgumentException ex){
+                System.out.println("Недопустимый синтаксис");
+                HelpFormatter hp = new HelpFormatter();
+                hp.printHelp("ciphxor",options);
             }
         }
     }
     public static void main(String args[]){
-
+        c.parse(args);
     }
 }
