c0a032eb9db443e2449ab21d3759125b5fefc4da
DaniilPavlov <cool.sharagin@mail.ru>
2018-03-26 15:58:19 +0300

    Need to repair classpath

---
+++ src/main/java/consol/Main.java
@@ -0,0 +1,25 @@
+package consol;
+
+import logic.Find;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+
+        boolean subDirectory = false;
+        String fileDirectory = new File("").getAbsolutePath();
+        String nameOfTheFile = args[args.length - 1];
+
+        for (int i = 0; i < args.length - 1; i++) {
+            if (args[i].equals("-r"))
+                subDirectory = true;
+            if (args[i].equals("-d"))
+                fileDirectory = args[i + 1];
+        }
+
+        Find find = new Find(fileDirectory, subDirectory);
+        System.out.println(find.find(nameOfTheFile));
+
+    }
+}
