064338e1f889778195560f507ae7596bd76280e4
DaniilPavlov <cool.sharagin@mail.ru>
2018-03-26 21:58:01 +0300

    One more thing

---
+++ src/main/java/logic/Find.java
@@ -0,0 +1,48 @@
+package logic;
+
+import java.io.File;
+
+public class Find {
+
+    private String fileName;
+
+    private Boolean subDirectory;
+
+
+    public Find(String fileName, Boolean subDirectory) {
+        this.fileName = fileName;
+        this.subDirectory = subDirectory;
+    }
+
+
+    private String search(String directory, String nameOfTheFile) {
+        String fullNameOfTheFile = directory + "/" + nameOfTheFile;
+        if (new File(fullNameOfTheFile).isFile()) return fullNameOfTheFile;
+        return "nothing";
+    }
+
+
+    private String subSearch(String directory, String nameOfTheFile) {
+        File ourFile = new File(directory);
+        String[] directories = ourFile.list();
+        if (directories != null)
+            for (int i = 0; i < directories.length; i++) {
+                String fullNameOfTheFile;
+                if (directories[i].equals(nameOfTheFile)) fullNameOfTheFile = directory;
+                else fullNameOfTheFile = directory + "/" + directories[i];
+                if (new File(fullNameOfTheFile + "/" + nameOfTheFile).isFile())
+                    return fullNameOfTheFile + "/" + nameOfTheFile;
+                fullNameOfTheFile = subSearch(fullNameOfTheFile, nameOfTheFile);
+                if (new File(fullNameOfTheFile).isFile()) return fullNameOfTheFile;
+            }
+        return "nothing";
+    }
+
+
+    public File find(String nameOfTheFile) {
+        String target;
+        if (subDirectory) target = subSearch(fileName, nameOfTheFile);
+        else target = search(fileName, nameOfTheFile);
+        return new File(target);
+    }
+}
