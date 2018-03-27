ded27b0bf9fcc94ce80cc3f9d2cb7ff9f3f7bd97
DaniilPavlov <cool.sharagin@mail.ru>
2018-03-27 14:08:46 +0300

    added tests for logic

---
+++ src/test/java/consol/MainTest.java
@@ -0,0 +1,15 @@
+package consol;
+
+import java.io.File;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void main() {
+
+    }
+
+}
---
+++ src/test/java/logic/FindTest.java
@@ -0,0 +1,34 @@
+package logic;
+
+import java.io.File;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindTest {
+
+    @Test
+    void main() {
+        Find path = new Find("C:\\Users\\loko\\IdeaProjects\\Find", true);
+
+        assertEquals(new File("C:\\Users\\loko\\IdeaProjects\\Find\\src\\test\\test1.txt"),
+                path.find("test1.txt"));
+
+        path = new Find("C:\\Users\\loko\\IdeaProjects\\Find", false);
+
+        assertEquals(new File("nothing"),
+                path.find("test1.txt"));
+
+        path = new Find("C:\\Users\\loko\\IdeaProjects\\Find", true);
+
+        assertEquals(new File("nothing"),
+                path.find("test3.txt"));
+
+        path = new Find("C:\\Users\\loko\\IdeaProjects\\Find\\src", false);
+
+        assertEquals(new File("C:\\Users\\loko\\IdeaProjects\\Find\\src\\test2.txt"),
+                path.find("test2.txt"));
+    }
+
+}
