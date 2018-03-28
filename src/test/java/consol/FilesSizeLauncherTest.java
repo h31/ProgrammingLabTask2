ef3792207a251ceb0c14f08a4d575c65b8020744
Виктор <sfera98@bk.ru>
2018-03-29 00:10:30 +0400

    update

--- src/test/java/consol/FilesSizeLauncherTest.java
+++ src/test/java/consol/FilesSizeLauncherTest.java
@@ -11,8 +11,8 @@ public class FilesSizeLauncherTest {
         String[] args1 = {"-c","-h","--si","src/test/java/filesForTest","src/test/java/filesForTest/TestFile1"};
         new FilesSizeLauncher().main(args1);
         String[] files = {"src/test/java/filesForTest","src/test/java/filesForTest/TestFile1"};
-        assertTrue(("12.442 kb\n" +
+        assertEquals("12.442 kb\n" +
                 "8.102 kb\n" +
-                "20.544 kb").equals(new FilesSize(true,true,true).filesSize(files)));
+                "20.544 kb",new FilesSize(true,true,true).filesSize(files));
     }
 }
