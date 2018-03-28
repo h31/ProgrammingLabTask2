cf63f4ea4cbe4a61dbc71f08e65c5269625c52e5
PolyaVyatkina <polya.vyatkina@mail.ru>
2018-03-28 19:32:51 +0300

    -

---
+++ spbstu/src/main/java/tail/interfaceTale.java
@@ -0,0 +1,25 @@
+package tail;
+
+import java.util.Scanner;
+
+public class interfaceTale {
+    public static void main(String[] args) throws Exception {
+        String name1 = "";
+        String name2 = "";
+        String inputCommandString;
+        //FileWriter writer = new FileWriter(name1);
+        //FileReader reader = new FileReader(name2);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите команду");
+        inputCommandString = scanner.nextLine();
+
+        if (inputCommandString.contains("-c") && inputCommandString.contains("-n"))
+            throw new IllegalArgumentException("Неверный формат данных");
+        String[] command = inputCommandString.split(" ");
+        String flag = command[1];
+
+        //writer.close();
+        //reader.close();
+    }
+}
---
+++ spbstu/src/main/java/tail/logicTail.java
@@ -0,0 +1,28 @@
+package tail;
+
+import java.io.*;
+import java.util.*;
+
+public class logicTail {
+
+    public List<String> c(int amountOfSymbols, List<String> linesFromFile) {
+        List<String> result = new ArrayList<String>();
+        int symbols = 0;
+        int i = linesFromFile.size() - 1;
+        while (symbols < amountOfSymbols) {
+            String Line = linesFromFile.get(i);
+            int lengthOfLine = Line.length();
+            if (lengthOfLine + symbols <= amountOfSymbols) result.add(0, Line);
+            else result.add(0, Line.substring(lengthOfLine - amountOfSymbols + symbols));
+            symbols += lengthOfLine;
+        }
+        return result;
+    }
+
+    public List<String> n(int amountOfLines, List<String> linesFromFile) {
+        List<String> result = new ArrayList<String>();
+        for (int i = linesFromFile.size() - amountOfLines; i < linesFromFile.size(); i++)
+            result.add(linesFromFile.get(i));
+        return result;
+    }
+}
