7b5a7db73ab3ec5077d29c3484f6e67c7682c098
Violetta-Mamykina <violetta-1998m@mail.ru>
2018-04-01 21:21:16 +0300

    corrected

--- ProgramLab2/src/test/java/UnitTest.java
+++ ProgramLab2/src/test/java/UnitTest.java
@@ -1,14 +1,13 @@
 import logic.Grep;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 class UnitTest {
-    Grep grep = new Grep("src\\test\\FileForRead");
+    private Grep grep = new Grep("src\\test\\FileForRead");
 
 
     @Test
@@ -25,7 +24,7 @@ class UnitTest {
 
     @Test
     void findExceptRegex() {
-        List<String> expResult = Arrays.asList(grep.getLines().get(16));
+        List<String> expResult = Collections.singletonList(grep.getLines().get(16));
         assertEquals(expResult, grep.findExceptRegex("/"));
     }
 }
