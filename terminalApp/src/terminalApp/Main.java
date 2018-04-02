f2637075d146f9d21fe11f5e18c73631fcac463f
evgenysobko <jooohn127@gmail.com>
2018-04-02 16:58:50 +0300

    Now it works!

--- terminalApp/src/terminalApp/Main.java
+++ terminalApp/src/terminalApp/Main.java
@@ -9,27 +9,24 @@ import java.util.Scanner;
 class Launcher {
 
     public static void main(String[] args) throws IOException {
-        String content = new String(Files.readAllBytes(Paths.get("terminalApp/text/file.txt")));
-        System.out.println(content);
-        Cut cutter = new Cut();
         Scanner question = new Scanner(System.in);
-        String cmd = question.nextLine();
-        switch (cmd) {
-            case "-c": {
-                Scanner values = new Scanner(System.in);
-                int n = values.nextInt();
-                int k = values.nextInt();
-                try (FileWriter writer = new FileWriter("terminalApp/text/fileNew.txt", false)) {
+        String cmd = question.next();
+        String oldFileName = question.next();
+        String newFileName = question.next();
+        int n = question.nextInt();
+        int k = question.nextInt();
+        String content = new String(Files.readAllBytes(Paths.get("terminalApp/text/" + oldFileName + ".txt")));
+        Cut cutter = new Cut();
+        switch(cmd) {
+            case "-c" : {
+                try (FileWriter writer = new FileWriter("terminalApp/text/" + newFileName + ".txt", false)) {
                     String newText = (cutter.cutterC(content, n, k).toString().replaceAll("\\[|\\]", ""));
                     writer.write(newText.replaceAll(", ", "\n"));
                 }
                 break;
             }
             case "-w": {
-                Scanner values = new Scanner(System.in);
-                int n = values.nextInt();
-                int k = values.nextInt();
-                try (FileWriter writer = new FileWriter("terminalApp/text/fileNew.txt", false)) {
+                try (FileWriter writer = new FileWriter("terminalApp/text/" + newFileName + ".txt", false)) {
                     String newText = (cutter.cutterW(content, n, k).toString().replaceAll("\\[|\\]", ""));
                     writer.write(newText.replaceAll(", ", "\n"));
                 }
