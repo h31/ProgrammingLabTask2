f18c2968c045c03e07e44c03e59ba141cb004e09
Виктор <sfera98@bk.ru>
2018-04-03 16:40:53 +0400

    Добавлены тесты на ловлю ошибок.

--- src/test/java/consol/FilesSizeLauncherTest.java
+++ src/test/java/consol/FilesSizeLauncherTest.java
@@ -1,13 +1,16 @@
 package consol;
 
 import logic.FilesSize;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.kohsuke.args4j.CmdLineException;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class FilesSizeLauncherTest {
+class FilesSizeLauncherTest {
     @Test
-    public void filesSizeLauncherTest() throws Exception {
+    void main() {
         String[] args1 = {"-c","-h","--si","src/test/java/filesForTest","src/test/java/filesForTest/TestFile1"};
         new FilesSizeLauncher().main(args1);
         String[] files = {"src/test/java/filesForTest","src/test/java/filesForTest/TestFile1"};
@@ -16,12 +19,17 @@ public class FilesSizeLauncherTest {
 
         String[] args2 = {"src/test/java/filesForTest","src/test/java/filesForTest/TestFile1"};
         new FilesSizeLauncher().main(args2);
-        assertEquals("12.150390625\n" + "7.912109375\n",
-                new FilesSize().filesTotalSize(files));
+        assertEquals("12.150390625\n" + "7.912109375\n", new FilesSize().filesTotalSize(files));
 
         String[] args3 = {"-h","src/test/java/filesForTest","src/test/java/filesForTest/TestFile1"};
         new FilesSizeLauncher().main(args3);
         assertEquals("12.150390625 kb\n" + "7.912109375 kb\n",
                 new FilesSize(false,true,false).filesTotalSize(files));
+
+        String[] argsForError = {"wdwdwd"};
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new FilesSize(false,true,true).filesTotalSize(argsForError);
+        });
     }
+
 }
--- src/test/java/logic/FilesSizeTest.java
+++ src/test/java/logic/FilesSizeTest.java
@@ -1,15 +1,18 @@
 package logic;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class FilesSizeTest {
-
+class FilesSizeTest {
     @Test
-    public void filesTotalSize() throws Exception {
+    void filesTotalSize() {
         String[] args = {"src/test/java/filesForTest/TestFile1","src/test/java/filesForTest/TestFile2",
                 "src/test/java/filesForTest/TestFile3"};
+        String[] argsForError = {"src/test/java/filesForTesting"};
+
         assertEquals("7.912109375\n" + "0.005859375\n" + "4.232421875\n",
                 new FilesSize().filesTotalSize(args));
         // -h
@@ -25,13 +28,17 @@ public class FilesSizeTest {
         assertEquals("7.912109375 kb\n" + "6.0 bytes\n" + "4.232421875 kb\n" + "12.150390625 kb",
                 new FilesSize(true,true,false).filesTotalSize(args));
         // -c -h --si
-         assertEquals("8.102 kb\n" + "6.0 bytes\n" + "4.334 kb\n" + "12.442 kb",
-                 new FilesSize(true,true,true).filesTotalSize(args));
-         // -c --si
-         assertEquals("8.102\n" + "0.006\n" + "4.334\n" + "12.442",
-                 new FilesSize(true,false,true).filesTotalSize(args));
+        assertEquals("8.102 kb\n" + "6.0 bytes\n" + "4.334 kb\n" + "12.442 kb",
+                new FilesSize(true,true,true).filesTotalSize(args));
+        // -c --si
+        assertEquals("8.102\n" + "0.006\n" + "4.334\n" + "12.442",
+                new FilesSize(true,false,true).filesTotalSize(args));
         // -h --si
         assertEquals("8.102 kb\n" + "6.0 bytes\n" + "4.334 kb\n",
                 new FilesSize(false,true,true).filesTotalSize(args));
+        // test for error detection
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new FilesSize(false,true,true).filesTotalSize(argsForError);
+        });
     }
 }
