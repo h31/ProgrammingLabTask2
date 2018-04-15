8134d90366080d86f76a10eae6da498c3c308ec9
DaniilPavlov <cool.sharagin@mail.ru>
2018-04-15 20:26:23 +0300

    Second corrections per day

--- src/main/java/console/FindLauncher.java
+++ src/main/java/console/FindLauncher.java
@@ -6,27 +6,27 @@ import java.io.File;
 
 public class FindLauncher {
 
-    static String[] consoleInput;
+    private String[] consoleInput;
 
     FindLauncher(String[] input) {
         consoleInput = input;
     }
 
-    static File isInputCorrect() throws Exception {
+    public File commandLine() throws Exception {
         if (consoleInput.length == 0)
-            throw new Exception("No input data available");
-        boolean useSubDirectoryForSearching = false;
+            throw new Exception("Not enough arguments on the command line");
+        boolean searchInSubDirectories = false;
         String fileDirectory = new File("").getAbsolutePath();
         String nameOfTheFile = consoleInput[consoleInput.length - 1];
         for (int i = 0; i < consoleInput.length - 1; i++) {
             if (consoleInput[i].equals("-r"))
-                useSubDirectoryForSearching = true;
+                searchInSubDirectories = true;
             if ((consoleInput[i].equals("-d")) && (consoleInput.length == 2)) {
                 throw new Exception("Incorrect input");
             } else if (consoleInput[i].equals("-d"))
                 fileDirectory = consoleInput[i + 1];
         }
-        Find find = new Find(fileDirectory, useSubDirectoryForSearching);
-        return new File(find.find(nameOfTheFile).toString());
+        Find find = new Find(fileDirectory, searchInSubDirectories);
+        return find.find(nameOfTheFile);
     }
 }
