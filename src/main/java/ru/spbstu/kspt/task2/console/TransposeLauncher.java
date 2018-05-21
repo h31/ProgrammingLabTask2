52fad9b7f1ec987f7530620dd447ce45f03c7f31
GSDTR <ilnur222@yahoo.com>
2018-05-21 22:00:36 +0300

    First try

---
+++ src/main/java/ru/spbstu/kspt/task2/console/TransposeLauncher.java
@@ -0,0 +1,44 @@
+package ru.spbstu.kspt.task2.console;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import ru.spbstu.kspt.task2.logic.Transpose;
+
+import java.nio.file.Path;
+
+public class TransposeLauncher {
+
+    @Argument(required = true)
+    private Path inputFile;
+
+    @Option(name = "-o")
+    private String outputFile;
+
+    @Option(name = "-a")
+    private int num;
+
+    @Option(name = "-t")
+    private boolean isCut;
+
+    @Option(name = "-r")
+    private boolean isRightSide;
+
+    public static void main(String[] args) {
+        new TransposeLauncher().launch(args);
+    }
+
+    public void launch(String[] args) {
+        CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            parser.printUsage(System.err);
+            return;
+        }
+        Transpose t = new Transpose(isRightSide, isCut, num, outputFile);
+        t.allLogic(inputFile);
+    }
+}
