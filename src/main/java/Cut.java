b3b691b87cd5c2cda2e19d8fa1cd75b5028afdac
evgenysobko <jooohn127@gmail.com>
2018-06-21 23:12:46 +0300

    ¯\_(ツ)_/¯

--- src/main/java/Cut.java
+++ src/main/java/Cut.java
@@ -5,47 +5,18 @@ public class Cut {
 
     public List<String> cutter(String text, int n, int k, boolean spaces) {
         List<String> newText = new ArrayList<>();
-        String textWithOrWithoutSpaces;
+        String textWithOrWithoutSpaces = "";
         if (spaces) {
             textWithOrWithoutSpaces = text;
         } else {
             textWithOrWithoutSpaces = text.replaceAll(" ", "");
         }
         for (String lines : textWithOrWithoutSpaces.split("\n")) {
-            StringBuilder newLine = new StringBuilder();
-            int length = lines.length();
-            if (n != 0) {
-                if (k >= length) {
-                    int begin = n;
-                    length -= n;
-                    while (length != 0) {
-                        newLine.append(lines.charAt(begin));
-                        begin++;
-                        length--;
-                    }
-                } else {
-                    length = k - n;
-                    int begin = n;
-                    while (length != 0) {
-                        newLine.append(lines.charAt(begin));
-                        begin++;
-                        length--;
-                    }
-                }
-            } else {
-                if (k >= length) {
-                    newLine.append(lines);
-                } else {
-                    length = k;
-                    int begin = n;
-                    while (length != 0) {
-                        newLine.append(lines.charAt(begin));
-                        begin++;
-                        length--;
-                    }
-                }
+            if (k > lines.length()) {
+                k = lines.length();
             }
-            newText.add(newLine.toString());
+            String newLine = lines.substring(n, k);
+            newText.add(newLine);
         }
         return newText;
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,26 +3,60 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
-class Launcher {
+class Main {
 
     public static void main(String[] args) throws IOException {
-        Scanner question = new Scanner(System.in);
-        String cmd = question.next();
-        String oldFileName = question.next();
-        String newFileName = question.next();
-        int n = question.nextInt();
-        int k = question.nextInt();
-        String content = new String(Files.readAllBytes(Paths.get("src/" + oldFileName + ".txt")));
+
         Cut cutter = new Cut();
         boolean spaces = false;
-        switch (cmd) {
-            case "-c": {
-                spaces = true;
-                break;
+        String oldFileName = "";
+        String newFileName = "";
+        int n = -1;
+        int k = 0;
+        Scanner scanner = new Scanner(System.in);
+
+
+        for (String arg : args) {
+            int i = 0;
+            switch (arg) {
+                case "-c": {
+                    spaces = true;
+                    break;
+                }
+                case "-o": {
+                    oldFileName = arg;
+                    break;
+                }
+                case "n-": {
+                    n = Integer.parseInt(arg);
+                    break;
+                }
+                case "-k": {
+                    k = Integer.parseInt(arg);
+                    break;
+                }
+                case "n-k": {
+                    String[] argument = arg.split("-");
+                    n = Integer.parseInt(argument[0]);
+                    k = Integer.parseInt(argument[1]);
+                    break;
+                }
             }
         }
+        if (Objects.equals(oldFileName, "")){
+            oldFileName = scanner.nextLine();
+            newFileName = scanner.nextLine();
+        }
+        if (n == -1) {
+            n = scanner.nextInt();
+        }
+        if (k == 0) {
+            k = scanner.nextInt();
+        }
+        String content = new String(Files.readAllBytes(Paths.get("src/" + oldFileName + ".txt")));
         try (FileWriter writer = new FileWriter("src/" + newFileName + ".txt", false)) {
             List<String> newText = cutter.cutter(content, n, k, spaces);
             String readyText = "";
@@ -31,7 +65,7 @@ class Launcher {
             }
             writer.write(readyText.replaceAll(", ", "\n"));
         }
-
     }
 }
+
 
